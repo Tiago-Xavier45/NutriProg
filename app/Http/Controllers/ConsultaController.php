85c00e738121b744122187f5411ef26e155ae532
Tiago-Xavier45 <tiago.xavier45@outlook.com>
2026-04-27 22:52:35 -0300

    feature: Numero celular puxando junto com nome do paciente

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -38,6 +38,7 @@ class ConsultaController extends Controller
             return [
                 'id' => (string) $cliente->id,
                 'name' => $cliente->name,
+                'phone' => $cliente->phone ?? '',
             ];
         });
 
